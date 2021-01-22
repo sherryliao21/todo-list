da2162f03d667d764df35d50141858166b598c0b
Sherry <sherry.c.liao@gmail.com>
2021-01-23 01:33:35 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -70,6 +70,15 @@ app.post('/todos/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+
+})
+
 app.listen(port, () => {
   console.log(`Express is listening on https://localhost:${port}`)
 })
