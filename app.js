154f5ea2d6286632d2bcc667835624bb95196b59
Sherry <sherry.c.liao@gmail.com>
2021-01-26 11:57:50 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   Todo.find() // get all data from Todo model
     .lean() // turn Mongoose model into JavaScript data array
+    .sort({ _id: 'asc' }) // 正序是 asc 反序的話就是desc
     .then(todos => res.render('index', { todos }))
     .catch(error => console.error(error))
 })
