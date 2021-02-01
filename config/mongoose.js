e17d54b6e771e78dab5398498b1d45cf3d4be79e
Sherry <sherry.c.liao@gmail.com>
2021-02-01 22:06:27 +0800

    fix: MONGODB_URI setting

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const db = mongoose.connection
-const MONGODB_URI = process.env.MOGODB_URI || 'mongodb://localhost/todo-list'
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/todo-list'
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
