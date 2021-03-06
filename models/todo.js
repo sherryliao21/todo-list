22446692a3606a8af2253a07aa3e8c2f7bfbab56
Sherry <sherry.c.liao@gmail.com>
2021-03-06 22:15:02 +0800

    feat: add user registration feature

--- models/todo.js
+++ models/todo.js
@@ -1,16 +1,14 @@
 const mongoose = require('mongoose')
-// set schema module
 const Schema = mongoose.Schema
 const todoSchema = new Schema({
   name: {
-    type: String, // data type is string
-    required: true, // this data is required
+    type: String,
+    required: true,
   },
   isDone: {
     type: Boolean,
     default: false, // 預設狀態為false(預設todo是未完成的)
   }
 })
-// export module
-// name the schema as 'Todo' 
+
 module.exports = mongoose.model('Todo', todoSchema)
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,12 +1,37 @@
 const express = require('express')
 const router = express.Router()
+const Users = require('../../models/user')
 
 router.get('/login', (req, res) => {
   return res.render('login')
 })
 
+router.post('/login', (req, res) => {
+
+})
+
 router.get('/register', (req, res) => {
   return res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  Users.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('User already exists.')
+        return res.render('register', {
+          name, email, password, confirmPassword
+        })
+      } else {
+        return Users.create({
+          name, email, password
+        })
+          .then(() => res.redirect('/'))
+          .catch(error => console.log(error))
+      }
+    })
+
 })
 
 module.exports = router
