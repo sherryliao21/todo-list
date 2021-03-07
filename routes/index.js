558e803599ba05399d78bd6ee3f7e829a043fa03
Sherry <sherry.c.liao@gmail.com>
2021-03-07 17:04:36 +0800

    feat: add user authentication in todo and home routers

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router()
 const home = require('./modules/home')
 const todos = require('./modules/todos')
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
 
-router.use('/', home)
-router.use('/todos', todos)
+// todos & home 頁面須要驗證過身分才能觀看內容，users本身就會要求登入驗證
+router.use('/todos', authenticator, todos)
 router.use('/users', users)
+router.use('/', authenticator, home) // 定義最寬鬆的路由放在最下方，就不會一直被攔截
 
 module.exports = router
