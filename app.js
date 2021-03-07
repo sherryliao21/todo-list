b4cdb391e58402b63ec577cbcf53e2104cf89787
Sherry <sherry.c.liao@gmail.com>
2021-03-07 15:07:03 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -8,13 +8,20 @@ const routes = require('./routes') // 預設會自動找到index.js檔案
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
+
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
