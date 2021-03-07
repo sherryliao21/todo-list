143642f1b82d3f9bbba178441b658727ddce6d99
Sherry <sherry.c.liao@gmail.com>
2021-03-07 15:43:09 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const usePassport = require('./config/passport')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -22,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
 
+usePassport(app)
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const Users = require('../../models/user')
+const passport = require('passport')
 
 router.get('/login', (req, res) => {
   return res.render('login')
 })
 
-router.post('/login', (req, res) => {
-
-})
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 router.get('/register', (req, res) => {
   return res.render('register')
