e0c89f00f74f4ef3463ae3313f455bac7d2fb02f
Sherry <sherry.c.liao@gmail.com>
2021-03-08 18:27:57 +0800

    feat: add login warning flash messages

--- config/passport.js
+++ config/passport.js
@@ -8,14 +8,19 @@ module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
   // setting up local strategy
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passReqToCallback: true
+  }, (req, email, password, done) => {
     User.findOne({ email })
       .then(user => {
         if (!user) {
-          return done(null, false, { message: 'This email is not registered.' })
+          req.flash('warning_msg', 'This email is not registered.')
+          return done(null, false)
         }
         if (user.password !== password) {
-          return done(null, false, { message: 'Email or password is incorrect.' })
+          req.flash('warning_msg', 'Email or password is incorrect.')
+          return done(null, false)
         }
         return done(null, user)
       })
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -9,7 +9,8 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
-  failureRedirect: '/users/login'
+  failureRedirect: '/users/login',
+  failureFlash: true
 }))
 
 router.get('/logout', (req, res) => {
