da0f295443889332a8a363818ed53719ec805e52
Sherry <sherry.c.liao@gmail.com>
2021-03-09 22:36:19 +0800

    feat: modify todoSeeder.js add new user && todo seed

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,11 +1,36 @@
+const bcrypt = require('bcryptjs')
+// mongodb connection requires MONGODB_URI, which we stored in .env
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const Todo = require('../todo') // import todo model
+const User = require('../user')
 const db = require('../../config/mongoose')
+// define seed user info
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678'
+}
 
 db.once('open', () => {
-  console.log('mongodb connected!')
-  // create script for adding 10 new data
-  for (let i = 0; i < 10; i++) {
-    Todo.create({ name: 'name-' + i })
-  }
-  console.log('done adding')
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    }))
+    .then(user => {
+      const userID = user._id
+      return Promise.all(Array.from(
+        { length: 10 },
+        (_, i) => Todo.create({ name: `name-${i}`, userID })
+      ))
+    })
+    .then(() => {
+      console.log('done.')
+      process.exit()
+    })
 })
